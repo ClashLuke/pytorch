be01ae7079499b3e4c4395bf0235031ada0d1395
Laith Sakka <lsakka@meta.com>
2024-11-07 10:28:07 -0800

    use has_free_unbacked_symbols
    ghstack-source-id: c9096ee254a67cc8ca730eebe61cd8586c7d3d2b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/140027

--- torch/fx/experimental/symbolic_shapes.py
+++ torch/fx/experimental/symbolic_shapes.py
@@ -768,6 +768,14 @@ def has_free_symbols(val: IterateExprs) -> bool:
     return not all(e.is_number for e in _iterate_exprs(val))
 
 
+def has_free_unbacked_symbols(x: IterateExprs) -> bool:
+    """Faster version of bool(free_unbacked_symbols(val))"""
+    for s in free_symbols(x):
+        if symbol_is_type(s, (SymT.UNBACKED_INT, SymT.UNBACKED_FLOAT)):
+            return True
+    return False
+
+
 # Like free_symbols, but filtered to only report unbacked symbols
 def free_unbacked_symbols(x: IterateExprs) -> OrderedSet[sympy.Symbol]:
     # NB: keep synced with is_unbacked_symint
